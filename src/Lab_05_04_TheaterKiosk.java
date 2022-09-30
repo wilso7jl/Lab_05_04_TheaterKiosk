60b2ad806a44edfb8a517b788e2849ba72544cb2
wilso <wilso7jl@mail.uc.edu>
2022-09-30 00:28:16 -0400

    Initial commit

---
+++ src/Lab_05_04_TheaterKiosk.java
@@ -0,0 +1,12 @@
+public class Lab_05_04_TheaterKiosk {
+    public static void main(String[] args) {
+        System.out.println("Please enter your age: ");
+        int customerAge = 25;
+        if(customerAge >= 21)
+        {
+            System.out.println("You get a wrist band!");
+        }
+
+
+    }
+}
